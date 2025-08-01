0a7de03f64eb5ebf6b5cdce942908f940207dcb0
Divya Omar <146424277+divyaomar@users.noreply.github.com>
2025-08-01 11:32:48 +0530

    Update todos.js
    just do

--- routes/todos.js
+++ routes/todos.js
@@ -4,8 +4,9 @@ const router = express.Router();
 
 //import controller
 const {createTodo} =require("../controllers/createTodo");
-
+const {getTodo} =require("../controllers/getTodo");
 //define api routes
 router.post("/createTodo", createTodo);
+router.post("/getTodo", getTodo);
 
 module.exports= router;
